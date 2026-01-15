da857f9cab87156fdb9f816480b690a0429ce2be
Adan <emanadan418@gmail.com>
2026-01-15 10:47:33 +0300

    adding first github action

---
+++ tests/test_users.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app.routers.main import app
+
+cilent = TestClient(app)
+
+def test_root():
+    res = cilent.get("/")
+    print(res.json().get('message'))
+
+
+
+
+
+
+#so yesterday in arabic class mehmet forgot his notebook under his table while we were going i gout up took his notebook and said mehmet he didn't hear i said mehemt again he looked back at me and said yeah i gave him his notebook he said oh thanks then later in bus we were talking about a secret of eman ok abdur rahman likes eman that was the secret and abdur rahman wanted eman to know so my borther was telling eman and emhmet wanted to know it too yani he was aking me please tell me what is it first i sid i can't then i said it's the same secret of me but happened with mean so he said ohh so abdur rahman likes herrr now i said yeah ut u don't kow about it yani i was telling him not to tell anyone he said yeah ofc i don't know about it and he made a face like  i am cooked today in class the teacher told us to write sentences with tha and sheen i wrote 2 but he wrote 5 the teacher told me to write more i said i can't so after 2 minutes mehmet told me write from the qurnan i said what he said write from the quran u will find many wallah think i said i can't think u think for me then he gave me an ayah and later i wrote it then in the bus time i wrote 10 sentences and gave it to him to check i said look i wrote this he read it then he said nicee and
+#and also after i showed mehmet my work on the notebook he said nice i took out a folded paper and asked him mehmet guess what is this he said i don't know i said guess then he looked at the paper he said oh it's and i gave him the paper(the paper was his drawing that i have from last year and he knows about it) he saw the drawing and he said oh its he took the drawing opened it smiled and gave it back to me then yeah so mehmet was telling his name to a new kid in the bus he said my name is mehmet  and i said no his is lying his name is ramzi he said what no then he realized what i meant by ramzi(last year we read a comprehension in arabic class and we had to role play so he was ramzi and i was someone else)  he said ohh yeah ramzii then the new kid asked about his age or something i don't remember and i added something like no he is lying blah blah i don't remeber and so mehmet like put his hand like covered my face with his hand not touched it just tried to cover it so i couldn't tell the kid and then i yani put his hand away like not rudely but i touched his hand i touched 2 fingers and put them away funily  
